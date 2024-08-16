25e8950f20ece74467f948396d6113754e0047e3
dduddo0226 <gusbk0112@gmail.com>
2024-08-16 09:20:21 +0900

    app key 추가

--- src/main/java/com/ss/sns/board/controller/ReplyController.java
+++ src/main/java/com/ss/sns/board/controller/ReplyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,6 +56,16 @@ public class ReplyController {
 			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 		}
 	}
-	
-	
+	@PutMapping("/{reply_no}")
+	public ResponseEntity<Void> updateReply(@PathVariable("reply_no")int reply_no, @RequestHeader("member_no") int memberNo,@RequestBody String replyContent){
+		Map<String, Object>hmap = new HashMap<String, Object>();
+		hmap.put("reply_no", reply_no);
+		hmap.put("member_no", memberNo);
+		hmap.put("reply_content", replyContent);
+		if (service.updateReply(hmap)) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		} else {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+		}
+	}
 }
