538e0ad10bb2be49e49c3e0fa30675338ca60ad2
mindrog <alsehdns@gmail.com>
2024-08-20 17:30:44 +0900

    카카오 공유 완성, 아이디찾기 완성

---
+++ src/main/java/com/ss/sns/trip/dto/shareBO.java
@@ -0,0 +1,14 @@
+package com.ss.sns.trip.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class shareBO {
+	int trip_no;
+	String member_nickname;
+	String trip_title;
+}
